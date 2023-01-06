49e40329baa08096cb55db1c7430e7ebe0ba9988
Mark Ibanez <mark.ibanez@gmail.com>
2023-01-06 17:50:05 +0800

    Create 00035 - Search Insert Position.ts

---
+++ problems/00035 - Search Insert Position.ts
@@ -0,0 +1,45 @@
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+// Example 1:
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
+
+// Example 2:
+// Input: nums = [1,3,5,6], target = 2
+// Output: 1
+
+// Example 3:
+// Input: nums = [1,3,5,6], target = 7
+// Output: 4
+
+
+// Constraints:
+// 1 <= nums.length <= 10^4
+// -10^4 <= nums[i] <= 10^4
+// nums contains distinct values sorted in ascending order.
+// -10^4 <= target <= 10^4
+
+function searchInsert(nums: number[], target: number): number {
+    let left = 0;
+    let right = nums.length - 1;
+    let result = nums.length;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (nums[mid] >= target) {
+            result = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return result;
+};
+
+// test cases
+console.log(searchInsert([1,3,5,6], 5)); // 2
+console.log(searchInsert([1,3,5,6], 2)); // 1
+console.log(searchInsert([1,3,5,6], 7)); // 4
