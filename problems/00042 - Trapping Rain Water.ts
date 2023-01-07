3bd1d6ad62a7a71d273c9dd96623097acc97edbd
Mark Ibanez <mark.ibanez@gmail.com>
2023-01-07 15:14:16 +0800

    Create 00042 - Trapping Rain Water.ts

---
+++ problems/00042 - Trapping Rain Water.ts
@@ -0,0 +1,45 @@
+// Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+// Example 1:
+// Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+// Output: 6
+// Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+
+// Example 2:
+// Input: height = [4,2,0,3,2,5]
+// Output: 9
+
+// Constraints:
+// n == height.length
+// 1 <= n <= 2 * 10^4
+// 0 <= height[i] <= 10^5
+
+function trap(height: number[]): number {
+    let left = 0;
+    let right = height.length - 1;
+    let leftMax = 0;
+    let rightMax = 0;
+    let result = 0;
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= leftMax) {
+                leftMax = height[left];
+            } else {
+                result += leftMax - height[left];
+            }
+            left++;
+        } else {
+            if (height[right] >= rightMax) {
+                rightMax = height[right];
+            } else {
+                result += rightMax - height[right];
+            }
+            right--;
+        }
+    }
+    return result;
+};
+
+// test cases
+console.log(trap([0,1,0,2,1,0,1,3,2,1,2,1])); // 6
+console.log(trap([4,2,0,3,2,5])); // 9
