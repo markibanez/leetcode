bac2ae4869cfed9a8f4efe71bb5dc8cfaa1c6055
Mark Ibanez <mark.ibanez@gmail.com>
2023-01-11 20:35:16 +0800

    Create 00065 - Valid Number.ts

---
+++ problems/00065 - Valid Number.ts
@@ -0,0 +1,86 @@
+// A valid number can be split up into these components (in order):
+// A decimal number or an integer.
+// (Optional) An 'e' or 'E', followed by an integer.
+
+// A decimal number can be split up into these components (in order):
+// (Optional) A sign character (either '+' or '-').
+// One of the following formats:
+// One or more digits, followed by a dot '.'.
+// One or more digits, followed by a dot '.', followed by one or more digits.
+// A dot '.', followed by one or more digits.
+
+// An integer can be split up into these components (in order):
+// (Optional) A sign character (either '+' or '-').
+// One or more digits.
+
+// For example, all the following are valid numbers: ["2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789"], while the following are not valid numbers: ["abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3", "95a54e53"].
+
+// Given a string s, return true if s is a valid number.
+
+// Example 1:
+// Input: s = "0"
+// Output: true
+
+// Example 2:
+// Input: s = "e"
+// Output: false
+
+// Example 3:
+// Input: s = "."
+// Output: false
+
+// Example 4:
+// Input: s = "1E9"
+// Output: true
+
+// Example 5:
+// Input: s = "e9"
+// Output: true
+
+// Example 6:
+// Input: s = "Infinity"
+// Output: false
+
+// Example 7:
+// Input: s = "6e6.5"
+// Output: false
+
+// Example 8:
+// Input: s = "-Infinity"
+// Output: false
+
+// Example 9:
+// Input: s = "+-."
+// Output: false
+
+// Constraints:
+// 1 <= s.length <= 20
+// s consists of only English letters (both uppercase and lowercase), digits (0-9), plus '+', minus '-', or dot '.'.
+
+function isNumber(s: string): boolean {
+    let exp = false, sign = false, num = false, dec = false
+    for (let c of s)
+        if (c >= '0' && c <= '9') num = true
+        else if (c === 'e' || c === 'E')
+            if (exp || !num) return false
+            else exp = true, sign = false, num = false, dec = false
+        else if (c === '+' || c === '-')
+            if (sign || num || dec) return false
+            else sign = true
+        else if (c === '.')
+            if (dec || exp) return false
+            else dec = true
+        else return false
+    return num
+}
+
+// test cases
+console.log(isNumber('0')); // true
+console.log(isNumber('e')); // false
+console.log(isNumber('.')); // false
+console.log(isNumber('1E9')); // true
+console.log(isNumber('e9')); // true
+console.log(isNumber('Infinity')); // false
+console.log(isNumber('6e6.5')); // false
+console.log(isNumber('-Infinity')); // false
+console.log(isNumber('+-.')); // false
