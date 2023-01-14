319f2908ca4d567517ce8b8be6750875d79ce575
Mark Ibanez <mark.ibanez@gmail.com>
2023-01-14 18:45:32 +0800

    Create 00074 - Search a 2D Matrix.ts

---
+++ problems/00074 - Search a 2D Matrix.ts
@@ -0,0 +1,41 @@
+// You are given an m x n integer matrix matrix with the following two properties:
+// Each row is sorted in non-decreasing order.
+// The first integer of each row is greater than the last integer of the previous row.
+// Given an integer target, return true if target is in matrix or false otherwise.
+
+// You must write a solution in O(log(m * n)) time complexity.
+
+// Example 1:
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+// Output: true
+
+// Example 2:
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+// Output: false
+
+// Constraints:
+// m == matrix.length
+// n == matrix[i].length
+// 1 <= m, n <= 100
+// -10^4 <= matrix[i][j], target <= 10^4
+
+function searchMatrix(matrix: number[][], target: number): boolean {
+    let row = 0;
+    let col = matrix[0].length - 1;
+    while (row < matrix.length && col >= 0) {
+        if (matrix[row][col] === target) {
+            return true;
+        } else if (matrix[row][col] > target) {
+            col--;
+        } else {
+            row++;
+        }
+    }
+    return false;
+};
+
+// test cases
+// @ts-ignore
+const tmatrix1 = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]];
+console.log(searchMatrix(tmatrix1, 3)); // true
+console.log(searchMatrix(tmatrix1, 13)); // false
