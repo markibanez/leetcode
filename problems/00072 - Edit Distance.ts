bdc966e20d8381d677515d3e12bfae6f4060c0e1
Mark Ibanez <mark.ibanez@gmail.com>
2023-01-14 18:39:24 +0800

    Create 00072 - Edit Distance.ts

---
+++ problems/00072 - Edit Distance.ts
@@ -0,0 +1,50 @@
+// Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+// You have the following three operations permitted on a word:
+// Insert a character
+// Delete a character
+// Replace a character
+
+// Example 1:
+// Input: word1 = "horse", word2 = "ros"
+// Output: 3
+// Explanation:
+// horse -> rorse (replace 'h' with 'r')
+// rorse -> rose (remove 'r')
+// rose -> ros (remove 'e')
+
+// Example 2:
+// Input: word1 = "intention", word2 = "execution"
+// Output: 5
+// Explanation:
+// intention -> inention (remove 't')
+// inention -> enention (replace 'i' with 'e')
+// enention -> exention (replace 'n' with 'x')
+// exention -> exection (replace 'n' with 'c')
+// exection -> execution (insert 'u')
+
+// Constraints:
+// 0 <= word1.length, word2.length <= 500
+// word1 and word2 consist of lowercase English letters.
+
+function minDistance(word1: string, word2: string): number {
+    let dp: number[][] = [];
+    for (let i = 0; i <= word1.length; i++) {
+        dp[i] = [];
+        for (let j = 0; j <= word2.length; j++) {
+            if (i === 0) {
+                dp[i][j] = j;
+            } else if (j === 0) {
+                dp[i][j] = i;
+            } else if (word1[i - 1] === word2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[word1.length][word2.length];
+};
+
+// test cases
+console.log(minDistance("horse", "ros")); // 3
+console.log(minDistance("intention", "execution")); // 5
