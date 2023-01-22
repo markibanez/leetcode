838bc98a8e480fe24cfa2894605ab43d09103e92
Mark Ibanez <mark.ibanez@gmail.com>
2023-01-22 20:19:33 +0800

    Create 00107 - Binary Tree Level Order Traversal II.ts

---
+++ problems/00107 - Binary Tree Level Order Traversal II.ts
@@ -0,0 +1,51 @@
+// Given the root of a binary tree, return the bottom-up level order traversal of its nodes' values. (i.e., from left to right, level by level from leaf to root).
+
+// Example 1:
+// Input: root = [3,9,20,null,null,15,7]
+// Output: [[15,7],[9,20],[3]]
+
+// Example 2:
+// Input: root = [1]
+// Output: [[1]]
+
+// Example 3:
+// Input: root = []
+// Output: []
+
+// Constraints:
+// The number of nodes in the tree is in the range [0, 2000].
+// -1000 <= Node.val <= 1000
+
+// @ts-ignore
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+};
+
+function levelOrderBottom(root: TreeNode | null): number[][] {
+    const result: number[][] = [];
+    if (!root) return result;
+    const queue = [root];
+    while (queue.length) {
+        const level = [];
+        const size = queue.length;
+        for (let i = 0; i < size; i++) {
+            const node = queue.shift()!;
+            level.push(node.val);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        result.unshift(level);
+    }
+    return result;
+};
+
+// test cases
+console.log(levelOrderBottom(new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7))))); // [[15,7],[9,20],[3]]
+console.log(levelOrderBottom(new TreeNode(1))); // [[1]]
