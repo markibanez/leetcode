8f690a831e37f98f9dd78236ea10c7eaa1d14132
Mark Ibanez <mark.ibanez@gmail.com>
2023-02-01 20:43:13 +0800

    Create 00113 - Path Sum II.ts

---
+++ problems/00113 - Path Sum II.ts
@@ -0,0 +1,47 @@
+// Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where the sum of the node values in the path equals targetSum. Each path should be returned as a list of the node values, not node references.
+// A root-to-leaf path is a path starting from the root and ending at any leaf node. A leaf is a node with no children.
+
+// Example 1:
+// Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+// Output: [[5,4,11,2],[5,8,4,5]]
+// Explanation: There are two paths whose sum equals targetSum:
+// 5 + 4 + 11 + 2 = 22
+// 5 + 8 + 4 + 5 = 22
+
+// Example 2:
+// Input: root = [1,2,3], targetSum = 5
+// Output: []
+
+// Example 3:
+// Input: root = [1,2], targetSum = 0
+// Output: []
+
+// Constraints:
+// The number of nodes in the tree is in the range [0, 5000].
+// -1000 <= Node.val <= 1000
+// -1000 <= targetSum <= 1000
+
+// @ts-ignore
+class TreeNode {
+    val: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+};
+
+function pathSum(root: TreeNode | null, targetSum: number): number[][] {
+    if (!root) return [];
+    if (!root.left && !root.right) return root.val === targetSum ? [[root.val]] : [];
+    const left = pathSum(root.left, targetSum - root.val);
+    const right = pathSum(root.right, targetSum - root.val);
+    return left.concat(right).map(path => [root.val].concat(path));
+};
+
+// test cases
+console.log(pathSum(new TreeNode(5, new TreeNode(4, new TreeNode(11, new TreeNode(7), new TreeNode(2))), new TreeNode(8, new TreeNode(13), new TreeNode(4, new TreeNode(5), new TreeNode(1)))), 22)); // [[5,4,11,2],[5,8,4,5]]
+console.log(pathSum(new TreeNode(1, new TreeNode(2), new TreeNode(3)), 5)); // []
+console.log(pathSum(new TreeNode(1, new TreeNode(2)), 0)); // []
