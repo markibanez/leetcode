1ac06f1346d6b7430806a231e71723cb18a7dd3e
Mark Ibanez <mark.ibanez@gmail.com>
2023-02-02 21:23:41 +0800

    Create 00119 - Pascal's Triangle II.ts

---
+++ problems/00119 - Pascal's Triangle II.ts
@@ -0,0 +1,35 @@
+// Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+// Example 1:
+// Input: rowIndex = 3
+// Output: [1,3,3,1]
+
+// Example 2:
+// Input: rowIndex = 0
+// Output: [1]
+
+// Example 3:
+// Input: rowIndex = 1
+// Output: [1,1]
+
+// Constraints:
+// 0 <= rowIndex <= 33
+
+// Follow up: Could you optimize your algorithm to use only O(rowIndex) extra space?
+
+function getRow(rowIndex: number): number[] {
+    const result = [1];
+    for (let i = 1; i <= rowIndex; i++) {
+        for (let j = i - 1; j > 0; j--) {
+            result[j] += result[j - 1];
+        }
+        result.push(1);
+    }
+    return result;
+};
+
+// test cases
+console.log(getRow(3)); // [1,3,3,1]
+console.log(getRow(0)); // [1]
+console.log(getRow(1)); // [1,1]
