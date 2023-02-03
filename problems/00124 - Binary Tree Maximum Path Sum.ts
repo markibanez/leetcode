ef304182e2949894133a5765fa16c0a21822b33f
Mark Ibanez <mark.ibanez@gmail.com>
2023-02-03 20:30:37 +0800

    Create 00124 - Binary Tree Maximum Path Sum.ts

---
+++ problems/00124 - Binary Tree Maximum Path Sum.ts
@@ -0,0 +1,47 @@
+// A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
+// The path sum of a path is the sum of the node's values in the path.
+// Given the root of a binary tree, return the maximum path sum of any non-empty path.
+
+// Example 1:
+// Input: root = [1,2,3]
+// Output: 6
+// Explanation: The optimal path is 2 -> 1 -> 3 with a path sum of 2 + 1 + 3 = 6.
+
+// Example 2:
+// Input: root = [-10,9,20,null,null,15,7]
+// Output: 42
+// Explanation: The optimal path is 15 -> 20 -> 7 with a path sum of 15 + 20 + 7 = 42.
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 3 * 10^4].
+// -1000 <= Node.val <= 1000
+
+// @ts-ignore
+class TreeNode {
+    val: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val);
+        this.left = (left===undefined ? null : left);
+        this.right = (right===undefined ? null : right);
+    }
+};
+
+function maxPathSum(root: TreeNode | null): number {
+    let max = -Infinity;
+    function dfs(node: TreeNode | null) {
+        if (!node) return 0;
+        let left = Math.max(0, dfs(node.left));
+        let right = Math.max(0, dfs(node.right));
+        max = Math.max(max, left + right + node.val);
+        return Math.max(left, right) + node.val;
+    }
+    dfs(root);
+    return max;
+};
+
+// test cases
+console.log(maxPathSum(new TreeNode(1, new TreeNode(2), new TreeNode(3)))); // 6
+console.log(maxPathSum(new TreeNode(-10, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7))))); // 42
