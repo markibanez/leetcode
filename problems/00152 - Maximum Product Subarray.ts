31244d3c3c28e384daa08bd3898e3a9637c6386d
Mark Ibanez <mark.ibanez@gmail.com>
2023-02-16 18:20:04 +0800

    Create 00152 - Maximum Product Subarray.ts

---
+++ problems/00152 - Maximum Product Subarray.ts
@@ -0,0 +1,35 @@
+// Given an integer array nums, find a subarray that has the largest product, and return the product.
+// The test cases are generated so that the answer will fit in a 32-bit integer.
+
+// Example 1:
+// Input: nums = [2,3,-2,4]
+// Output: 6
+// Explanation: [2,3] has the largest product 6.
+
+// Example 2:
+// Input: nums = [-2,0,-1]
+// Output: 0
+// Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+
+// Constraints:
+// 1 <= nums.length <= 2 * 10^4
+// -10 <= nums[i] <= 10
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+function maxProduct(nums: number[]): number {
+    let max = nums[0];
+    let min = nums[0];
+    let result = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        let num = nums[i];
+        let tempMax = Math.max(num, num * max, num * min);
+        min = Math.min(num, num * max, num * min);
+        max = tempMax;
+        result = Math.max(result, max);
+    }
+    return result;
+};
+
+// test cases
+console.log(maxProduct([2, 3, -2, 4])); // 6
+console.log(maxProduct([-2, 0, -1])); // 0
