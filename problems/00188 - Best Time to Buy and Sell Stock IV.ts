5851e369dea6601e7517f29c718661c807efe625
Mark Ibanez <mark.ibanez@gmail.com>
2023-02-27 18:51:47 +0800

    Create 00188 - Best Time to Buy and Sell Stock IV.ts

---
+++ problems/00188 - Best Time to Buy and Sell Stock IV.ts
@@ -0,0 +1,41 @@
+// You are given an integer array prices where prices[i] is the price of a given stock on the ith day, and an integer k.
+// Find the maximum profit you can achieve. You may complete at most k transactions.
+// Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+// Example 1:
+// Input: k = 2, prices = [2,4,1]
+// Output: 2
+// Explanation: Buy on day 1 (price = 2) and sell on day 2 (price = 4), profit = 4-2 = 2.
+
+// Example 2:
+// Input: k = 2, prices = [3,2,6,5,0,3]
+// Output: 7
+// Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4. Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+
+// Constraints:
+// 1 <= k <= 100
+// 1 <= prices.length <= 1000
+// 0 <= prices[i] <= 1000
+
+function maxProfit(k: number, prices: number[]): number {
+    if (k >= prices.length / 2) {
+        let profit = 0;
+        for (let i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) profit += prices[i] - prices[i - 1];
+        }
+        return profit;
+    }
+    const dp = new Array(k + 1).fill(0).map(() => new Array(prices.length).fill(0));
+    for (let i = 1; i <= k; i++) {
+        let max = -prices[0];
+        for (let j = 1; j < prices.length; j++) {
+            dp[i][j] = Math.max(dp[i][j - 1], prices[j] + max);
+            max = Math.max(max, dp[i - 1][j - 1] - prices[j]);
+        }
+    }
+    return dp[k][prices.length - 1];
+};
+
+// test cases
+console.log(maxProfit(2, [2,4,1])); // 2
+console.log(maxProfit(2, [3,2,6,5,0,3])); // 7
