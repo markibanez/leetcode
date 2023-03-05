46a80a98a27a33c73a916ad2ce52335ffe53df51
Mark Ibanez <mark.ibanez@gmail.com>
2023-03-05 16:00:54 +0800

    Create 00217 - Contains Duplicate.ts

---
+++ problems/00217 - Contains Duplicate.ts
@@ -0,0 +1,26 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+// Example 1:
+// Input: nums = [1,2,3,1]
+// Output: true
+
+// Example 2:
+// Input: nums = [1,2,3,4]
+// Output: false
+
+// Example 3:
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+// Constraints:
+// 1 <= nums.length <= 10^5
+// -10^9 <= nums[i] <= 10^9
+
+function containsDuplicate(nums: number[]): boolean {
+    return new Set(nums).size !== nums.length;
+};
+
+// test cases
+console.log(containsDuplicate([1,2,3,1])); // true
+console.log(containsDuplicate([1,2,3,4])); // false
+console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2])); // true
