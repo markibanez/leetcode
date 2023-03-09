551bbb075a3d7eeaa81026d7efdc7046181128a1
Mark Ibanez <mark.ibanez@gmail.com>
2023-03-09 14:44:27 +0800

    Create 00229 - Majority Element II.ts

---
+++ problems/00229 - Majority Element II.ts
@@ -0,0 +1,61 @@
+// Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
+
+// Example 1:
+// Input: nums = [3,2,3]
+// Output: [3]
+
+// Example 2:
+// Input: nums = [1]
+// Output: [1]
+
+// Example 3:
+// Input: nums = [1,2]
+// Output: [1,2]
+
+// Constraints:
+// 1 <= nums.length <= 5 * 10^4
+// -10^9 <= nums[i] <= 10^9
+
+// Follow up: Could you solve the problem in linear time and in O(1) space?
+
+function majorityElement(nums: number[]): number[] {
+    let count1 = 0, count2 = 0, candidate1 = 0, candidate2 = 1;
+    for (let num of nums) {
+        if (num === candidate1) {
+            count1++;
+        } else if (num === candidate2) {
+            count2++;
+        } else if (count1 === 0) {
+            candidate1 = num;
+            count1 = 1;
+        } else if (count2 === 0) {
+            candidate2 = num;
+            count2 = 1;
+        } else {
+            count1--;
+            count2--;
+        }
+    }
+    count1 = 0;
+    count2 = 0;
+    for (let num of nums) {
+        if (num === candidate1) {
+            count1++;
+        } else if (num === candidate2) {
+            count2++;
+        }
+    }
+    const result: number[] = [];
+    if (count1 > nums.length / 3) {
+        result.push(candidate1);
+    }
+    if (count2 > nums.length / 3) {
+        result.push(candidate2);
+    }
+    return result;
+};
+
+// test cases
+console.log(majorityElement([3,2,3])); // [3]
+console.log(majorityElement([1])); // [1]
+console.log(majorityElement([1,2])); // [1,2]
