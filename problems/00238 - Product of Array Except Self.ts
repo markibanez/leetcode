ff6e3b5074c29f8836e32c4e0a8a4c6fabe1b9ee
Mark Ibanez <mark.ibanez@gmail.com>
2023-03-12 10:35:04 +0800

    Create 00238 - Product of Array Except Self.ts

---
+++ problems/00238 - Product of Array Except Self.ts
@@ -0,0 +1,35 @@
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+// You must write an algorithm that runs in O(n) time and without using the division operation.
+
+// Example 1:
+// Input: nums = [1,2,3,4]
+// Output: [24,12,8,6]
+
+// Example 2:
+// Input: nums = [-1,1,0,-3,3]
+// Output: [0,0,9,0,0]
+
+// Constraints:
+// 2 <= nums.length <= 10^5
+// -30 <= nums[i] <= 30
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+// Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
+
+function productExceptSelf(nums: number[]): number[] {
+    const result = new Array(nums.length).fill(1);
+    let left = 1;
+    let right = 1;
+    for (let i = 0; i < nums.length; i++) {
+        result[i] *= left;
+        left *= nums[i];
+        result[nums.length - 1 - i] *= right;
+        right *= nums[nums.length - 1 - i];
+    }
+    return result;
+}
+
+// test cases
+console.log(productExceptSelf([1, 2, 3, 4])); // [24, 12, 8, 6]
+console.log(productExceptSelf([-1, 1, 0, -3, 3])); // [0, 0, 9, 0, 0]
