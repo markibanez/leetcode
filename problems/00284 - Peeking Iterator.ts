530ef6b079d203c0652829a72944fb120f83f42a
Mark Ibanez <mark.ibanez@gmail.com>
2023-03-14 16:15:20 +0800

    Create 00284 - Peeking Iterator.ts

---
+++ problems/00284 - Peeking Iterator.ts
@@ -0,0 +1,77 @@
+// Design an iterator that supports the peek operation on an existing iterator in addition to the hasNext and the next operations.
+// Implement the PeekingIterator class:
+// PeekingIterator(Iterator<int> nums) Initializes the object with the given integer iterator iterator.
+// int next() Returns the next element in the array and moves the pointer to the next element.
+// boolean hasNext() Returns true if there are still elements in the array.
+// int peek() Returns the next element in the array without moving the pointer.
+// Note: Each language may have a different implementation of the constructor and Iterator, but they all support the int next() and boolean hasNext() functions.
+
+// Example 1:
+// Input
+// ["PeekingIterator", "next", "peek", "next", "next", "hasNext"]
+// [[[1, 2, 3]], [], [], [], [], []]
+// Output
+// [null, 1, 2, 2, 3, false]
+
+// Explanation
+// PeekingIterator peekingIterator = new PeekingIterator([1, 2, 3]); // [1,2,3]
+// peekingIterator.next();    // return 1, the pointer moves to the next element [1,2,3].
+// peekingIterator.peek();    // return 2, the pointer does not move [1,2,3].
+// peekingIterator.next();    // return 2, the pointer moves to the next element [1,2,3]
+// peekingIterator.next();    // return 3, the pointer moves to the next element [1,2,3]
+// peekingIterator.hasNext(); // return False
+
+// Constraints:
+// 1 <= nums.length <= 1000
+// 1 <= nums[i] <= 1000
+// All the calls to next and peek are valid.
+// At most 1000 calls will be made to next, hasNext, and peek.
+
+// Follow up: How would you extend your design to be generic and work with all types, not just integer?
+
+// This is the Iterator's API interface.
+// You should not implement it, or speculate about its implementation
+// class Iterator {
+//      hasNext(): boolean {}
+//      next(): number {}
+// }
+
+class PeekingIterator {
+    // @ts-ignore
+    private iterator: Iterator;
+    private peeked: number | undefined;
+
+    // @ts-ignore
+    constructor(iterator: Iterator) {
+        this.iterator = iterator;
+        this.peeked = undefined;
+    }
+
+    peek(): number {
+        if (this.peeked === undefined) {
+            this.peeked = this.iterator.next();
+        }
+        return this.peeked!;
+    }
+
+    next(): number {
+        if (this.peeked !== undefined) {
+            const result = this.peeked;
+            this.peeked = undefined;
+            return result;
+        }
+        return this.iterator.next();
+    }
+
+    hasNext(): boolean {
+        return this.peeked !== undefined || this.iterator.hasNext();
+    }
+}
+
+// test cases
+const peekingIterator = new PeekingIterator([1, 2, 3][Symbol.iterator]());
+console.log(peekingIterator.next()); // 1
+console.log(peekingIterator.peek()); // 2
+console.log(peekingIterator.next()); // 2
+console.log(peekingIterator.next()); // 3
+console.log(peekingIterator.hasNext()); // false
