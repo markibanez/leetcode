3b1e1cc3928f3447cc71d8af4dde55f7e6c9e717
Mark Ibanez <mark.ibanez@gmail.com>
2023-03-22 07:40:24 +0800

    Create 00326 - Power of Three.ts

---
+++ problems/00326 - Power of Three.ts
@@ -0,0 +1,33 @@
+// Given an integer n, return true if it is a power of three. Otherwise, return false.
+// An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+// Example 1:
+// Input: n = 27
+// Output: true
+// Explanation: 27 = 33
+
+// Example 2:
+// Input: n = 0
+// Output: false
+// Explanation: There is no x where 3x = 0.
+
+// Example 3:
+// Input: n = -1
+// Output: false
+// Explanation: There is no x where 3x = (-1).
+
+// Constraints:
+// -2^31 <= n <= 2^31 - 1
+
+function isPowerOfThree(n: number): boolean {
+  if (n < 1) return false;
+  while (n % 3 === 0) {
+    n /= 3;
+  }
+  return n === 1;
+}
+
+// test cases
+console.log(isPowerOfThree(27)); // true
+console.log(isPowerOfThree(0)); // false
+console.log(isPowerOfThree(-1)); // false
